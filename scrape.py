464b2157c95e187ebbb153b15f5222196a293e6d
vmathi-at-837322754217 <vigneshwaraarasu.mathi@wavicledata.com>
2021-12-24 21:15:03 +0530

    Added requirements and logging

--- scrape.py
+++ scrape.py
@@ -23,26 +23,31 @@ cleaner.style = True
 
 @app.route('/')
 def url_scrape():
-    response={}
-    url = request.args.get('url')
-    page = requests.get(url)
-    content = page.text
-    doc = document_fromstring(content)
-    content = lxml.html.tostring(cleaner.clean_html(doc))
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', str(content))
-    cleantags = re.compile('&.*?;')
-    cleantexts = re.sub(cleantags, '', cleantext)
-    _RE_COMBINE_WHITESPACE = re.compile(r"(?a:\s+)")
-    parsetext = re.sub(_RE_COMBINE_WHITESPACE, ' ', cleantexts)
-    parsetext = parsetext.replace("\n", "")
-    parsetext = parsetext.replace("\r", "")
-    parsetext = parsetext.replace("\\'", "'")
-    parsetext = parsetext.replace("\\n", "")
-    parsetext = parsetext.replace("\\t", "")
-    response['url']=url
-    response['url_content']=parsetext
-    response['url_content_timestamp']=datetime.now().utcnow()
-    return response
+    try:
+        print('successefully entered scrape request')
+        response={}
+        url = request.args.get('url')
+        page = requests.get(url)
+        content = page.text
+        doc = document_fromstring(content)
+        content = lxml.html.tostring(cleaner.clean_html(doc))
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', str(content))
+        cleantags = re.compile('&.*?;')
+        cleantexts = re.sub(cleantags, '', cleantext)
+        _RE_COMBINE_WHITESPACE = re.compile(r"(?a:\s+)")
+        parsetext = re.sub(_RE_COMBINE_WHITESPACE, ' ', cleantexts)
+        parsetext = parsetext.replace("\n", "")
+        parsetext = parsetext.replace("\r", "")
+        parsetext = parsetext.replace("\\'", "'")
+        parsetext = parsetext.replace("\\n", "")
+        parsetext = parsetext.replace("\\t", "")
+        response['url']=url
+        response['url_content']=parsetext
+        response['url_content_timestamp']=datetime.now().utcnow()
+        return response
+        
+    except:
+        return "Error in request"
 
 app.run(debug = False)
